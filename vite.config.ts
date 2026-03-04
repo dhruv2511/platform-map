6d1bc17252bf8a24b7dc9c7c6d1ef9a8f4baceec
Dhruvil Thakkar <dhruvilthakkar2511@gmail.com>
2026-03-04 16:45:57 +0000

    adding capability to host the platform map as gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
